ba23291200ffbedb213803df5b7c5ca2d1c85709
Daniel Band <daniel@5dlab.com>
2014-09-01 18:19:50 +0200

    Initial Commit

---
+++ sample-ios/sample_ios.h
@@ -0,0 +1,13 @@
+//
+//  sample_ios.h
+//  sample-ios
+//
+//  Created by Daniel Band on 01/09/14.
+//  Copyright (c) 2014 5dlab GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface sample_ios : NSObject
+
+@end
---
+++ sample-ios/sample_ios.m
@@ -0,0 +1,13 @@
+//
+//  sample_ios.m
+//  sample-ios
+//
+//  Created by Daniel Band on 01/09/14.
+//  Copyright (c) 2014 5dlab GmbH. All rights reserved.
+//
+
+#import "sample_ios.h"
+
+@implementation sample_ios
+
+@end
